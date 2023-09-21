1a0da79410c8d4e4838e32b2398739993d2d52c6
Vicente BERENGUER <vibelo.vbl@gmail.com>
2023-09-21 16:55:17 +0000

    backend deleted

--- backend/test.py
+++
@@ -1,5 +0,0 @@
-from ldap3 import Server, Connection, ALL
-server = Server('10.156.0.10', get_info=ALL)
-con = Connection(server,'cn=Administrator,cn=Users,dc=test,dc=local', 'Vibelo.93!', auto_bind=True)
-con.search('dc=test,dc=local', '(objectclass=person)',attributes=['unicodePwd', 'objectclass'])
-con.entries[0]
